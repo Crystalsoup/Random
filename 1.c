3cef6d1c57db17aa8a855e29e13a5bdf7a617847
Crystal <113200390+Crystalsoup@users.noreply.github.com>
2023-01-19 21:14:38 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
-    // for loop terminates when num is less than count
+    
     for(count = 1; count <= num; ++count)
     {
         sum += count;
